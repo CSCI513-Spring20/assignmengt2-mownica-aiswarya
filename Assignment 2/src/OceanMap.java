e2367bdc02ea7b138877faa1c03e3e497041aae0
aiswaryagurram <60040075+aiswaryagurram@users.noreply.github.com>
2020-02-16 16:57:06 -0600

    Initial Message

---
+++ Assignment 2/src/OceanMap.java
@@ -0,0 +1,35 @@
+import java.awt.Point;
+import java.util.Random;
+
+public class OceanMap  {
+
+	static int x,y;
+	static int Isx,Isy;
+	static Random r=new Random();
+	boolean[][] oceanGrid=new boolean[10][10];
+	public boolean[][] getMap()
+	{
+		return oceanGrid;
+	}
+	public static Point getShipLocation() {
+			 x=r.nextInt(10);
+			 y=r.nextInt(10);
+		return new Point(x,y);
+		
+	 
+	}
+	
+	public static Point getislandslocation() {
+		 Isx=r.nextInt(10);
+		 Isy=r.nextInt(10);
+		 if(x != Isx || y != Isy)
+		 {
+		 return new Point(Isx,Isy);
+		 }
+		 else
+		 {
+			 return new Point(0,0);
+		 }
+		 
+	}
+}
---
+++ Assignment 2/src/Ship.java
@@ -0,0 +1,95 @@
+import java.awt.Point;
+import javafx.scene.layout.AnchorPane;
+
+public class Ship {
+	 int X,Y;
+	 Point currentLocation;
+	 AnchorPane root;
+	 public Ship(int x, int y) {
+
+			X = x;
+			Y = y;
+			
+		}
+	 
+	public Ship(OceanMap oceanMap) {
+		// TODO Auto-generated constructor stub
+	}
+	public void goWest(int[] IslocX,int[] IslocY) 
+	{
+		// TODO Auto-generated method stub
+		Boolean iscellempty = true;
+	if (X != 0)
+		{
+		for(int i=0;i<IslocX.length;i++)
+		{
+			if(IslocX[i] != 0 && IslocY[i] != 0 && X-1 == IslocX[i] && Y == IslocY[i])
+			{
+				iscellempty =false;
+				break;
+			}
+			
+		}
+		if(iscellempty)
+			X--;
+		}
+	}
+	public void goSouth(int[] IslocX,int[] IslocY) {
+		// TODO Auto-generated method stub
+		Boolean iscellempty = true;
+		if (Y != 9)
+		{
+			for(int i=0;i<IslocY.length;i++)
+			{
+				if(IslocX[i] != 0 && IslocY[i] != 0 && X == IslocX[i] && Y+1 == IslocY[i])
+				{
+					iscellempty =false;
+					break;
+				}
+			}
+			if(iscellempty)
+			Y++;
+		}
+	}
+	public void goNorth(int[] IslocX,int[] IslocY) {
+		// TODO Auto-generated method stub
+		Boolean iscellempty = true;
+		if (Y != 0)
+		{
+			for(int i=0;i<IslocY.length;i++)
+			{
+				if(IslocX[i] != 0 && IslocY[i] != 0 && X == IslocX[i] && Y-1 == IslocY[i])
+				{
+					iscellempty =false;
+					break;
+				}
+			}
+			if(iscellempty)
+			Y--;
+		}
+	}
+	public void goEast(int[] IslocX,int[] IslocY) 
+	{
+		// TODO Auto-generated method stub
+		Boolean iscellempty = true;
+		if (X != 9)
+		{
+			for(int i=0;i<IslocX.length;i++)
+			{
+				if(IslocX[i] != 0 && IslocY[i] != 0 && X+1 == IslocX[i] && Y == IslocY[i])
+				{
+					iscellempty =false;
+					break;
+				}
+			}
+			if(iscellempty)
+			X++;
+		}
+	}
+	public Point getShipLocation() {
+		// TODO Auto-generated method stub
+		return new Point(X,Y);
+	}
+	
+
+}
